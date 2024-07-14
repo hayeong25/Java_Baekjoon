b67503af31bf72879e6cf94c5185b6978e1ae105
hayeong25 <hayeong0624@gmail.com>
2024-07-14 17:30:00 +0900

    A/B (1008)

---
+++ src/main/java/com/coding/baekjoon/입출력과사칙연산/BOJ_1008.java
@@ -0,0 +1,14 @@
+package com.coding.baekjoon.입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class BOJ_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+
+        System.out.println(a / b);
+    }
+}
