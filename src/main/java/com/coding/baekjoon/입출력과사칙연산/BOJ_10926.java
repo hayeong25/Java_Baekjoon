2fa34209ded5dbb98b330d88cf1558747eadb341
hayeong25 <hayeong0624@gmail.com>
2024-07-15 20:40:00 +0900

    ??! (10926)

---
+++ src/main/java/com/coding/baekjoon/입출력과사칙연산/BOJ_10926.java
@@ -0,0 +1,13 @@
+package com.coding.baekjoon.입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class BOJ_10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        System.out.println(str + "??!");
+    }
+}
