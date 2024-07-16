b9879ae8bdc8a8b0a79251765b7ed888d5d9f318
hayeong25 <hayeong0624@gmail.com>
2024-07-16 20:30:00 +0900

    1998년생인 내가 태국에서는 2541년생?! (18108)

---
+++ src/main/java/com/coding/baekjoon/입출력과사칙연산/BOJ_18108.java
@@ -0,0 +1,13 @@
+package com.coding.baekjoon.입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class BOJ_18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int y = sc.nextInt();
+
+        System.out.println(y - 543);
+    }
+}
