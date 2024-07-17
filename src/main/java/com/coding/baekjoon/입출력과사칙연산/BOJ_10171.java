bbce28d5e6b8e8a4d4662337194bade17366e069
hayeong25 <hayeong0624@gmail.com>
2024-07-17 21:00:00 +0900

    고양이 (10171)

---
+++ src/main/java/com/coding/baekjoon/입출력과사칙연산/BOJ_10171.java
@@ -0,0 +1,10 @@
+package com.coding.baekjoon.입출력과사칙연산;
+
+public class BOJ_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                           " )  ( ')\n" +
+                           "(  /  )\n" +
+                           " \\(__)|");
+    }
+}
