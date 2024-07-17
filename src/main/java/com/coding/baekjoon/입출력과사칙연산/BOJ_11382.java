435bafd89e2459091bf14887e3be883921485ef9
hayeong25 <hayeong0624@gmail.com>
2024-07-17 20:50:00 +0900

    꼬마 정민 (11382)

---
+++ src/main/java/com/coding/baekjoon/입출력과사칙연산/BOJ_11382.java
@@ -0,0 +1,15 @@
+package com.coding.baekjoon.입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class BOJ_11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        System.out.println(a + b + c);
+    }
+}
