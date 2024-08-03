85fe4cc7afcd2053c5edb5a1170320e0bf559bbd
hayeong25 <hayeong0624@gmail.com>
2024-08-03 15:10:00 +0900

    단어 길이 재기 (2743)

---
+++ src/main/java/com/coding/baekjoon/문자열/BOJ_2743.java
@@ -0,0 +1,15 @@
+package com.coding.baekjoon.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = reader.readLine();
+
+        System.out.println(s.length());
+    }
+}
