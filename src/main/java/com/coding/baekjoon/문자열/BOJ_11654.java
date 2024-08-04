1eb4da0c91086f68e60d196e340ca07f8e0ac884
hayeong25 <hayeong0624@gmail.com>
2024-08-04 13:30:00 +0900

    아스키 코드 (11654)

---
+++ src/main/java/com/coding/baekjoon/문자열/BOJ_11654.java
@@ -0,0 +1,13 @@
+package com.coding.baekjoon.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println((int)reader.readLine().charAt(0));
+    }
+}
