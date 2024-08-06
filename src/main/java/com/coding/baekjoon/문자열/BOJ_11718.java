b5e8001c541d909a1bd0414f2f2e862ac1071c61
hayeong25 <hayeong0624@gmail.com>
2024-08-06 21:10:00 +0900

    그대로 출력하기 (11718)

---
+++ src/main/java/com/coding/baekjoon/문자열/BOJ_11718.java
@@ -0,0 +1,17 @@
+package com.coding.baekjoon.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String s;
+
+        while ((s = reader.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
+}
