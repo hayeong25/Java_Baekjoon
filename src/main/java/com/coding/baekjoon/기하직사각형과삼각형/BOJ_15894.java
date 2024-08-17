0b5481448d6318bfef23b9af93d413368432ce1a
hayeong25 <hayeong0624@gmail.com>
2024-08-17 15:50:43 +0900

    수학은 체육과목 입니다 (15894)

---
+++ src/main/java/com/coding/baekjoon/기하직사각형과삼각형/BOJ_15894.java
@@ -0,0 +1,15 @@
+package com.coding.baekjoon.기하직사각형과삼각형;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_15894 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(reader.readLine());
+
+        System.out.println(n * 4);
+    }
+}
