edd7007a8a07c54ed08dcfdd2b11c88f8b920670
hayeong25 <hayeong0624@gmail.com>
2024-08-19 15:30:44 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 2 (24263)

---
+++ src/main/java/com/coding/baekjoon/시간복잡도/BOJ_24263.java
@@ -0,0 +1,14 @@
+package com.coding.baekjoon.시간복잡도;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(reader.readLine());
+        System.out.println(1);
+    }
+}
